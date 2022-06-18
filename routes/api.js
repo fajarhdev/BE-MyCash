ea9d852b12e045b49124da1ad1edb377aea3056d
Alhazen4 <cybernonk404@gmail.com>
2022-06-18 19:55:35 +0700

    Delete brick

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const { token } = require("../brick/brick");
+// const { token } = require("../brick/brick");
+var request = require('request');
 
 router.get("/", (req, res, next) => {
-	res.json(token);
-	console.log(`From API: ${token}`);
+	request({
+		method: 'GET',
+		url: 'https://sandbox.onebrick.io/v1/auth/token',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': 'Basic MDQ5M2YxMDItNzg3Mi00MzQ1LWJkYzgtYTk0NDJiYWEzMzAwOjhJVFd1OGYwS0NCYkowYUhmdUZvamN4YTBvTHQ5dw=='
+		}}, function (error, response, body) {
+		// console.log('Status:', response.statusCode);
+		// console.log('Headers:', JSON.stringify(response.headers));
+		// console.log('Response:', body);
+	
+		const raw = JSON.parse(body);
+		const { data } = raw;
+		// const token = JSON.stringify(data.access_token);
+		const token = data.access_token;
+		// console.log('Public_Token:', token);
+		// module.exports = { token };
+		res.json(token);
+	});
 });
 
 module.exports = router;
